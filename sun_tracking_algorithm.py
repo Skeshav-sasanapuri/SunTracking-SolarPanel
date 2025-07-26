555998439ee72198ca8ffebb6b32a4c7b58e0138
keshav-sasanapuri <keshavsasanapuri@gmail.com>
2025-07-26 14:39:02 -0400

    install libraries, import packages, setup requirements, and create .gitignore

---
+++ sun_tracking_algorithm.py
@@ -0,0 +1,10 @@
+from pvlib.location import Location  # To get solar Altitude and Azimuth
+import pandas as pd
+
+"""
+This program helps track the location of the sun in the sky given the date, time, location, and altitude
+It then gets the sun vector
+Using this sun vector, it helps align the normal of the solar panel supported by 4 pistons to this sun vector thereby 
+enabling maximum efficiency
+"""
+
